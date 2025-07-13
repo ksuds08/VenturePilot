059aaa38b6b4bc55034f7e6bc990e77411f77df9
ksuds08 <65447814+ksuds08@users.noreply.github.com>
2025-07-13 03:56:25 -0700

    Create index.js

---
+++ worker/index.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const path = url.pathname.slice(1);
+
+    switch (path) {
+      case 'check_user_status':
+        return new Response(JSON.stringify({ status: 'ok' }), {
+          headers: { 'Content-Type': 'application/json' }
+        });
+      // TODO: Add other tool endpoints (planBusiness, generateProduct, etc.)
+      default:
+        return new Response('Not found', { status: 404 });
+    }
+  }
+};
