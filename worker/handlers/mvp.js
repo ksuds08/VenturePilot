d70eb72d5c5f0db842c3afcf7e4da25e04093f05
ksuds08 <65447814+ksuds08@users.noreply.github.com>
2025-07-25 22:35:10 -0700

    Update mvp.js

--- worker/handlers/mvp.js
+++ worker/handlers/mvp.js
@@ -1,4 +1,4 @@
-/ SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 //
 // Main handler for the `/mvp` endpoint.  This module orchestrates the
 // generation of MVP code by invoking the planning, decomposition and
